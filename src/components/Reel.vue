7638d937bdadd0d7bc582ee4b21c83feff77f0d9
Artur Faiazov <faartur@gmail.com>
2021-09-13 00:35:54 +0300

    Add simple reels spinning

--- src/components/Reel.vue
+++ src/components/Reel.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="reel">
+  <div class="reel" ref="el">
     <img class="reel__item" v-for="symbol in symbols" :key="symbol.key" :src="symbol.image" :alt="symbol.key">
   </div>
 </template>
-<script>
+<script lang="ts">
+import mojs from '@mojs/core/dist/mo.umd.js'
+import { defineComponent, ref, watch } from 'vue'
 import TripleBar from '@/assets/reel-symbols/3xBAR.png'
 import Bar from '@/assets/reel-symbols/BAR.png'
 import DoubleBar from '@/assets/reel-symbols/2xBAR.png'
@@ -32,23 +34,66 @@ const symbols = [
     image: Cherry
   },
 ]
-export default {
+
+export default defineComponent({
   name: 'Reel',
-  setup() {
+  props: {
+    elementIndex: {
+      type: Number,
+      default: 0
+    },
+    delay: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup(props) {
+    const el = ref(null)
+    const rotateBase = 360 / symbols.length;
+    watch(props, () => {
+      const randomDeg = rotateBase * props.elementIndex
+      const html = new mojs.Html({
+        el: el.value,
+        y: -70,
+        rotateX: { 360: 0 },
+        isShowEnd: true,
+        isForce3d: false,
+        duration: 500,
+        delay: 0,
+        repeat: 4,
+        speed: 1,
+        easing: 'linear.none',
+      }).then({
+        y: -70,
+        rotateX: -randomDeg,
+        isShowEnd: true,
+        isForce3d: false,
+        duration: 500 * props.delay,
+        delay: 0,
+        repeat: 0,
+        speed: 1,
+        easing: 'linear.none',
+      })
+
+      html.play()
+    })
     return {
+      el,
       symbols
     }
   }
-}
+})
 </script>
 
 <style lang="scss" scoped>
 @use 'sass:math';
+
 $item-count: 5;
 $item--height: 200px;
 
 $radius: $item--height / (2 * math.tan(math.$pi / $item-count));
 $radius-out: $item--height / (2 * math.sin(math.$pi / $item-count));
+$rotate-base: 360deg / $item-count;
 
 .reel {
   display: grid;
@@ -56,7 +101,7 @@ $radius-out: $item--height / (2 * math.sin(math.$pi / $item-count));
   position: relative;
   width: 200px;
   height: $radius-out * 2;
-  transform: translateY(-($radius-out - $item--height / 2)) rotateX(0deg);
+  transform: translateY(-($radius-out - $item--height / 2));
 
   &__item {
     height: $item--height;
@@ -64,10 +109,8 @@ $radius-out: $item--height / (2 * math.sin(math.$pi / $item-count));
     top: $radius-out - $item--height / 2;
     backface-visibility: hidden;
 
-    $rotate-base: 360deg / $item-count;
     @for $i from 0 to $item-count + 1 {
       &:nth-child(#{$i + 1}) {
-        @debug $i;
         transform: rotateX($rotate-base * $i) translateZ($radius);
       }
     }
--- src/components/SlotsGame.vue
+++ src/components/SlotsGame.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="reels">
-    <Reel />
-    <Reel />
-    <Reel />
+    <Reel ref="reel" :elementIndex="centered[0]" :delay="0" />
+    <Reel ref="reel" :elementIndex="centered[1]" :delay="1" />
+    <Reel ref="reel" :elementIndex="centered[2]" :delay="2" />
   </div>
+  <button @click="spinHandler">Spin</button>
 </template>
 
-<script>
+<script lang="ts">
 import Reel from "./Reel.vue"
+import { defineComponent, ref } from "vue"
 
-export default {
-  name: "SlotsGame",
-  components: {Reel}
+function randomIntFromInterval(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
+
+export default defineComponent({
+  name: "SlotsGame",
+  components: {
+    Reel
+  },
+  setup() {
+    const reel = ref(null)
+    const centered = ref([])
+    const spinHandler = () => {
+      centered.value = [
+        randomIntFromInterval(1, 5),
+        randomIntFromInterval(1, 5),
+        randomIntFromInterval(1, 5)
+      ]
+    }
+
+    return {
+      reel,
+      centered,
+      spinHandler
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped>
